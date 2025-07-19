48a4e38a18ec777b509491d4c9436189a4ff0f57
ramyageesala <geesalaramya6@gmail.com>
2025-07-19 15:57:37 +0530

    Add files via upload

---
+++ manager.js
@@ -0,0 +1,28 @@
+const students = [];
+function AddStudent(){
+    const name = document.getElementById("nameInput").value;
+    const age = parseInt(document.getElementById("ageInput").value);
+    const course = document.getElementById("courseInput").value;
+    if(!name || !age || !course){
+        alert("Please enter all fields");
+        return;
+    }
+    const student = {
+        name: name,
+        age: age,
+        course: course
+
+    };
+    students.push(student);
+    displayStudents();
+}
+function displayStudents(){
+    const studentList=document.getElementById("studentList");
+    studentList.innerHTML = "";
+    students.forEach((student,index) => {
+    const div = document.createElement("div");
+    div.className="student";
+    div.innerHTML=`<strong>${index+1}.${student.name}</strong>(age:${student.age},Course:${student.course})`;
+    studentList.appendChild(div);
+    });
+}
